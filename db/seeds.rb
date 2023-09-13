b8101f7336aed589b1955e52178bc3e7796b205c
Sergio Brocos <sergiobrocos@gmail.com>
2023-09-13 19:20:40 +0200

    update seed for custom the organization

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # You can remove the 'faker' gem if you don't want Decidim seeds.
-Decidim.seed!
+
+Decidim.seed! if Decidim::Organization.count.zero?
+
+# Custom Organization
+organization = Decidim::Organization.first
+
+return if organization.name == 'Sabadell'
+
+general_handler = 'sabadell_city'
+udpate_data = {
+  name: 'Sabadell',
+  twitter_handler: general_handler,
+  facebook_handler: general_handler,
+  instagram_handler: general_handler,
+  youtube_handler: general_handler,
+  github_handler: 'sbrocos',
+  # in local don't change it because if change localhost, doesn't run correctly
+  host: ENV.fetch("DECIDIM_HOST", "localhost"),
+}
+
+organization.update(udpate_data) 
+puts 'Organization updated.'
+
